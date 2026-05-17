748eaf8a77d52f85684f9e7b33f9220f9ea0151f
katkasoft <katkaleks@gmail.com>
2026-05-17 18:46:14 +0300

    displaying error in article_display.rs

--- src/articles_display.rs
+++ src/articles_display.rs
@@ -3,6 +3,7 @@ use rocket::serde::Serialize;
 use rocket_dyn_templates::{Template, context};
 use sqlx::FromRow;
 use crate::db::DbPool;
+use rocket::http::Status;
 
 #[derive(Serialize, FromRow)]
 #[serde(crate = "rocket::serde")]
@@ -12,22 +13,24 @@ struct Article {
 }
 
 #[get("/article/<id>")]
-pub async fn article(id: u32, pool: &State<DbPool>) -> Option<Template> {
+pub async fn article(id: u32, pool: &State<DbPool>) -> Result<Template, Status> {
     let result = sqlx::query_as::<_, Article>("SELECT title, content FROM articles WHERE id = ?")
         .bind(id)
         .fetch_optional(pool.inner())
         .await;
     match result {
-        Ok(Some(article)) => Some(Template::render("article", context! {
-            title: article.title,
-            content: article.content
-        })),
+        Ok(Some(article)) => {
+            Ok(Template::render("article", context! {
+                title: article.title,
+                content: article.content
+            }))
+        }
         Ok(None) => {
-            None //TODO: сделать тут отображение ошибки
-        },
+            Err(Status::NotFound)
+        }
         Err(e) => {
-            eprintln!("database error: {}", e); //TODO: сделать тут отображение ошибки
-            None
+            eprintln!("Database error: {:?}", e);
+            Err(Status::InternalServerError)
         }
     }
 }
