b2672a24b61a3cb4bb9472ccbd85e2ceafa872b6
katkasoft <katkaleks@gmail.com>
2026-06-30 18:38:06 +0300

    displaying articles at main page (backend)

--- src/pages.rs
+++ src/pages.rs
@@ -1,8 +1,33 @@
 use rocket_dyn_templates::{Template, context};
+use crate::db::DbPool;
+use rocket::State;
+use sqlx::Row;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct Article {
+    pub id: i64,
+    pub title: String,
+    pub content: String,
+}
 
 #[get("/")]
-pub fn index() -> Template {
-    Template::render("index", context! {})
+pub async fn index(pool: &State<DbPool>) -> Template {
+    let rows = sqlx::query("SELECT id, title, content FROM articles")
+        .fetch_all(&**pool)
+        .await
+        .expect("Ошибка при получении статей, ты криворукий");
+
+    let articles: Vec<Article> = rows
+        .into_iter()
+        .map(|row| Article {
+            id: row.get(0),
+            title: row.get(1),
+            content: row.get(2),
+        })
+        .collect();
+
+    Template::render("index", context! { rows: articles })
 }
 
 #[get("/new")]
