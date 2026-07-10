95ec33bda5664ab291b501c2af946ee778ff7708
katkasoft <katkaleks@gmail.com>
2026-07-10 12:57:20 +0300

    upvotes frontend

---
+++ static/js/article.js
@@ -0,0 +1,29 @@
+let voted = false;
+let score = 0;
+const upvoteBtn = document.querySelector('.vote-up');
+const downvoteBtn = document.querySelector('.vote-down');
+const voteScore = document.getElementById('vote-score');
+
+const content = document.getElementById('content');
+document.addEventListener("DOMContentLoaded", () => {
+    content.innerHTML = marked.parse(content.innerHTML);
+    document.getElementById('edit-btn').addEventListener('click', () => {
+        const id = window.location.pathname.split('/').at(-1);
+        window.location.href = `/edit?id=${id}`;
+    });
+    upvoteBtn.addEventListener('click', () => vote('up'));
+    downvoteBtn.addEventListener('click', () => vote('down'));
+});
+
+function vote(option) {
+    if (voted) return;
+    if (option === 'up') {
+        upvoteBtn.classList.add('active');
+        score += 1;
+    } else if (option === 'down') {
+        downvoteBtn.classList.add('active');
+        score -= 1;
+    }
+    voted = true;
+    voteScore.textContent = score;
+}
